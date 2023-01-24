7a2631c54c3ecbf9564fe161806b2ebdc4cbe9fa
nickjbrowning <nickjbrowning@gmail.com>
2023-01-24 15:44:01 +0100

    x

--- tensor_contraction/tensor_contraction/_tests/numba_tests.py
+++ tensor_contraction/tensor_contraction/_tests/numba_tests.py
@@ -13,16 +13,16 @@ os.environ['NUMBA_ENABLE_CUDASIM'] = "1"
 start = torch.cuda.Event(enable_timing=True)
 end = torch.cuda.Event(enable_timing=True)
 
-X = np.fromfile('X.npy').reshape(21, 128, 16)
-Y = np.fromfile('Y.npy').reshape(21, 3)
-U3 = np.fromfile('U_3.npy').reshape(16,16,16,23)
-W3 = np.fromfile('W_3.npy').reshape(3,23,128)
+X = np.fromfile('../../data/X.npy').reshape(21, 128, 16)
+Y = np.fromfile('../../data/Y.npy').reshape(21, 3)
+U3 = np.fromfile('../../data/U_3.npy').reshape(16,16,16,23)
+W3 = np.fromfile('../../data/W_3.npy').reshape(3,23,128)
 
-U2 = np.fromfile('U_2.npy').reshape(16,16, 4)
-W2 = np.fromfile('W_2.npy').reshape(3,4,128)
+U2 = np.fromfile('../../data/U_2.npy').reshape(16,16, 4)
+W2 = np.fromfile('../../data/W_2.npy').reshape(3,4,128)
 
-U1 = np.fromfile('U_1.npy').reshape(16,1)
-W1 = np.fromfile('W_1.npy').reshape(3,1,128)
+U1 = np.fromfile('../../data/U_1.npy').reshape(16,1)
+W1 = np.fromfile('../../data/W_1.npy').reshape(3,1,128)
 
 U3_torch = torch.from_numpy(U3).cuda()
 W3_torch =  torch.from_numpy(W3).cuda()
@@ -33,16 +33,12 @@ W2_torch =  torch.from_numpy(W2).cuda()
 U1_torch = torch.from_numpy(U1).cuda()
 W1_torch =  torch.from_numpy(W1).cuda()
 
-W_2 = np.fromfile('W_2.npy').reshape(3,4,128)
 X_torch = torch.from_numpy(X.reshape(21, 128, 16)).float().cuda()
 Y_torch = torch.from_numpy(Y).float().cuda()
-
-
 
 indices = torch.where(U3_torch !=0)
 U3_sparse = U3_torch[indices].cpu().numpy()
 U3_sparse_indices = torch.nonzero(U3_torch).cpu().numpy()
-
 
 U3_sparse = cuda.to_device(U3_sparse)
 U3_sparse_indices = cuda.to_device(U3_sparse_indices)
@@ -52,11 +48,6 @@ X = cuda.to_device(X.reshape(21, 16, 128))
 
 U3_non_sparse = np.zeros((3, U3.shape[0], U3.shape[1]), dtype=np.float32)
 U3_non_sparse_indices = np.zeros((2, 3, U3.shape[0], U3.shape[1]), dtype=np.int32)
-
-print (U3[1,1, 0])
-print (U3[1,1, 6])
-print (U3[1,1, 8])
-
 
 for i in range(U3.shape[0]):
     for j in range(U3.shape[1]):
@@ -89,10 +80,6 @@ W_dim_2 = math.ceil(W3.shape[2]/threads_per_block[1])
 out_u3w3x = np.zeros((21, 16, 16, 128))
 out_u3w3x = cuda.to_device(out_u3w3x)
 
-print (Y)
-
-print (W_dim_1, W_dim_2)
-
 atom_types = np.array([1,1,1,1,1,1,1,2,2,2,1,1,2,0,0,0,0,0,0,0,0])
 atom_types = cuda.to_device(atom_types)
 
@@ -105,7 +92,6 @@ equation_contract = "bc...i,bci->bc..."
 
 UW = contract('...ik, ekc -> ...iec', U_tensors[3],W_tensors[3])
 
-print (UW.shape)
 UWX = contract('...iec, bci -> bec...', UW,X_torch)
 
 
@@ -152,10 +138,7 @@ def sparse_accumulate_U3W(U3_non_sparse, U3_non_sparse_indices, W, out):
     
     cuda.syncthreads()
 
-    for element in range(3): 
-    
-        #if (bx == i_val and tx == 0 and ty == 0):   
-        #    print (W[element, 11, 0], W_shared[11, 0])
+    for element in range(3):
                   
         for r in range(3):
             
@@ -186,37 +169,7 @@ print ("kernel time %.3f ms" % ((end - start) * 1000.0))
 
 hout = out.copy_to_host()
 
-#print (UW[0, :, :, 0, :])
-#print (hout[0, :, :, 0, :])
-
 diff= UW.cpu().numpy() - hout 
-
-print ("----")
-print (U3[i_val,j_val,k_val])
-print (U3_non_sparse.copy_to_host()[:, i_val,j_val])
-print (U3_non_sparse_indices.copy_to_host()[..., i_val,j_val])
-
-print ("weights:")
-print (W3[0, l_val, 0])
-print (W3[1, l_val, 0])
-print (W3[2, l_val, 0])
-
-print ()
-
-test = U3[i_val,j_val,k_val, l_val] * W3[:, l_val, :]
-
-print (hout[i_val,j_val,k_val, 0])
-print ("--test--")
-
-print (test[0])
-print (UW[i_val,j_val,k_val, 0])
-
-idx, jdx, kdx, edx, ldx = np.where(diff>  1e-5)
-
-print (idx)
-print (jdx)
-print (kdx)
-print (edx)
 
 sparse_accumulate_U3W3X[(21, 16), (16,16)](U3_non_sparse, U3_non_sparse_indices, W3,X, atom_types, out_u3w3x)
 
@@ -226,12 +179,6 @@ for i in range (50):
     torch.cuda.synchronize()
     end = time()
 
-print (out_contract)
-print (out_u3w3x.copy_to_host())
 print ("U3U3X kernel time %.3f ms" % ((end - start) * 1000.0))
 
-#for i in range(5):
-#    print (idx[i*128 * 3], jdx[i*128*3], kdx[i*128*3] )
-
-
-
+print ("hello")
