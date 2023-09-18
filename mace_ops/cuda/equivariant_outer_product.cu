737577ac12a289018779e593a49279daa3fa42cd
Nick J. Browning <nickjbrowning@gmail.com>
2023-09-19 01:36:02 +0200

    x

--- mace_ops/cuda/equivariant_outer_product.cu
+++ mace_ops/cuda/equivariant_outer_product.cu
@@ -271,11 +271,11 @@ __global__ void equivariant_outer_product_forward_kernel(const torch::PackedTens
             int32_t x_idx = buffer_mu_1[instruction_idx];
             int32_t y_idx = buffer_mu_2[instruction_idx];
             int32_t out_idx = buffer_mu_3[instruction_idx];
-
+            
             for (int32_t edge_idx = 0; edge_idx < nedges; edge_idx++)
             {
-                int edge = edge_start + edge_idx;
 
+                int edge = edge_start + edge_idx;
                 scalar_t x = 0.0;
 
                 if (valid)
@@ -302,158 +302,6 @@ __global__ void equivariant_outer_product_forward_kernel(const torch::PackedTens
         }
     }
 }
-
-/*
-
-template <typename scalar_t>
-__global__ void equivariant_outer_product_forward2_kernel(const torch::PackedTensorAccessor64<scalar_t, 3, torch::RestrictPtrTraits> X,
-                                                          const torch::PackedTensorAccessor64<scalar_t, 2, torch::RestrictPtrTraits> Y,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> receiver_list,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> neighbour_indices,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> mu_1,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> mu_2,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> mu_3,
-                                                          const torch::PackedTensorAccessor64<scalar_t, 1, torch::RestrictPtrTraits> cg_coefficients,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> warp_indices,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> warp_nwork,
-                                                          const torch::PackedTensorAccessor64<int32_t, 1, torch::RestrictPtrTraits> output_ordering,
-                                                          torch::PackedTensorAccessor64<scalar_t, 3, torch::RestrictPtrTraits> output)
-{
-    extern __shared__ char buffer[];
-
-    void *sptr = buffer;
-    size_t space = 0;
-
-
-    // scalar_t *buffer_x = shared_array<scalar_t>(blockDim.x * X.size(1), sptr, &space);
-    scalar_t *buffer_out = shared_array<scalar_t>(blockDim.x * output.size(1), sptr, &space);
-    // scalar_t *buffer_y = shared_array<scalar_t>(Y.size(1), sptr, &space);
-    scalar_t *buffer_cg_coefficients = shared_array<scalar_t>(mu_1.size(0), sptr, &space);
-
-    int32_t *buffer_mu_1 = shared_array<int32_t>(mu_1.size(0), sptr, &space);
-    int32_t *buffer_mu_2 = shared_array<int32_t>(mu_1.size(0), sptr, &space);
-    int32_t *buffer_mu_3 = shared_array<int32_t>(mu_1.size(0), sptr, &space);
-
-    int32_t *buffer_index_start = shared_array<int32_t>(warp_nwork.size(0), sptr, &space);
-    int32_t *buffer_nwork = shared_array<int32_t>(warp_nwork.size(0), sptr, &space);
-
-    int32_t *buffer_output_ordering = shared_array<int32_t>(output.size(1), sptr, &space);
-
-
-    int32_t edge_start = neighbour_indices[blockIdx.x];
-    int32_t edge_end = 0;
-
-    int32_t node_index = receiver_list[edge_start]; // get the idnex of the node we need to sum into.
-
-    if (blockIdx.x == neighbour_indices.size(0) - 1) // nnodes -1
-    {
-        edge_end = Y.size(0); // nedges -1
-    }
-    else
-    {
-        edge_end = neighbour_indices[blockIdx.x + 1];
-    }
-
-    int32_t nedges = edge_end - edge_start;
-
-    // check if this node has neighbours
-    if (nedges == 0)
-    {
-        return;
-    }
-
-    int32_t feat = blockIdx.y * blockDim.x + threadIdx.x;
-
-    bool valid = feat < X.size(2);
-
-    // load shared memory buffers...
-    if (threadIdx.y == 0)
-    {
-        for (int32_t i = threadIdx.x; i < mu_1.size(0); i += blockDim.x)
-        {
-            buffer_mu_1[i] = mu_1[i];
-            buffer_mu_2[i] = mu_2[i];
-            buffer_mu_3[i] = mu_3[i];
-            buffer_cg_coefficients[i] = cg_coefficients[i];
-        }
-
-        for (int32_t i = threadIdx.x; i < warp_indices.size(0); i += blockDim.x)
-        {
-            buffer_index_start[i] = warp_indices[i];
-            buffer_nwork[i] = warp_nwork[i];
-        }
-
-        for (int32_t i = threadIdx.x; i < output.size(1); i += blockDim.x)
-        {
-            buffer_output_ordering[i] = output_ordering[i];
-        }
-    }
-
-    __syncthreads();
-
-    // zero out shared memory
-    for (int32_t i = threadIdx.y; i < output.size(1); i += blockDim.y)
-    {
-        buffer_out[i * blockDim.x + threadIdx.x] = 0.0;
-    }
-
-    __syncthreads();
-
-    for (int32_t instruction = 0; instruction < warp_indices.size(0); instruction++)
-    {
-        int32_t index_start = buffer_index_start[instruction];
-        int32_t nwork = buffer_nwork[instruction];
-
-        for (int32_t j = 0; j < nwork; j++)
-        {
-            int32_t instruction_idx = index_start + j;
-
-            int32_t x_idx = buffer_mu_1[instruction_idx];
-            int32_t y_idx = buffer_mu_2[instruction_idx];
-            int32_t out_idx = buffer_mu_3[instruction_idx];
-
-            scalar_t cg_coeff = buffer_cg_coefficients[instruction_idx];
-
-            for (int i = 0; i < n; i++)
-            {
-                int edge = i * blockDim.x + threadIdx.x;
-
-                scalar_t y = 0.0;
-                scalar_t x = 0.0;
-
-                if (edge < nedges)
-                {
-                    x =  X[edge_start + edge][x_idx][feat];
-                }
-            }
-            for (int32_t edge_idx = threadIdx.x; edge_idx < nedges; edge_idx += blockDim.x)
-            {
-                // int edge = edge_start + edge_idx;
-
-                scalar_t x = 0.0;
-
-                if (valid)
-                    x = X[edge][x_idx][feat];
-
-                scalar_t y = Y[edge][y_idx];
-
-                atomicAdd(&buffer_out[out_idx * blockDim.x + threadIdx.x], cg_coeff * x * y);
-            }
-        }
-    }
-
-    __syncthreads();
-
-    for (int i = threadIdx.y; i < output.size(1); i += blockDim.y)
-    {
-        if (valid)
-        {
-            int order = buffer_output_ordering[i];
-            // output[node_index][order][feat] = buffer_out[i * blockDim.x + threadIdx.x];
-            output[node_index][i][feat] = buffer_out[order * blockDim.x + threadIdx.x];
-        }
-    }
-} */
 
 torch::Tensor equivariant_outer_product_forward_gpu(torch::Tensor X,
                                                     torch::Tensor Y,
