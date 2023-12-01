b8b7643b31a5d9f5c4882e70363669ae4aa4a852
Nicholas Browning <nickjbrowning@gmail.com>
2023-12-01 13:48:56 +0100

    comment

--- examples/InvariantMessagePassing.py
+++ examples/InvariantMessagePassing.py
@@ -299,4 +299,4 @@ def benchmark(dtype, device):
     
 if __name__ == "__main__":
     accuracy(torch.float32, "cuda")
-    #benchmark(torch.float32, "cuda")
+    #benchmark(torch.float32, "cuda") # run this with nsys nvprof python3 examples/InvariantMessagePassing.py
