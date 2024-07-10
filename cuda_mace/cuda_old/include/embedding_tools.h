07ea420fcbf27f00a4e71d84add271810a1d962a
Nick J. Browning <nickjbrowning@gmail.com>
2024-07-11 00:05:56 +0200

    remove unused folder

--- cuda_mace/cuda_old/include/embedding_tools.h
+++
@@ -1,7 +0,0 @@
-#include <torch/script.h>
-#include <vector>
-
-using namespace std;
-
-std::vector<torch::Tensor> parse_elemental_embedding_v1(torch::Tensor elemental_embedding);
-std::vector<torch::Tensor> parse_elemental_embedding_v2(torch::Tensor elemental_embedding);
