5f68ee210da3b17e24eb8082f7e8650ec7d311ce
Nick J. Browning <nickjbrowning@gmail.com>
2024-11-27 12:05:02 +0100

    update sys path

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ class cmake_ext(build_ext):
 
         cmake_options = [
             f"-DCMAKE_INSTALL_PREFIX={install_dir}"
-            #f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
         ]
 
         CUDA_HOME = os.environ.get("CUDA_HOME")
