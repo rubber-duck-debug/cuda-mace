34575cc55d12f669c84b5817dbc083f3020c721a
Nick J. Browning <nickjbrowning@gmail.com>
2024-12-01 17:40:42 +0100

    added jit wrapper for utility fn.

--- cuda_mace/cuda/src/invariant_message_passing_impl.cu
+++ cuda_mace/cuda/src/invariant_message_passing_impl.cu
@@ -6,14 +6,10 @@
 #define NELEMENTS_PER_BLOCK 512
 
 __global__ void calculate_first_occurences_kernel_ptr(
-    const int *__restrict__ receiver_list,
-    const int nelements_input,
-    const int nelements_output,
-    const int * sort_idx,
-    const bool use_sort,
-    int * __restrict__ first_occurences_start,
-    int * __restrict__ first_occurences_end)
-{
+    const int *__restrict__ receiver_list, const int nelements_input,
+    const int nelements_output, const int *sort_idx, const bool use_sort,
+    int *__restrict__ first_occurences_start,
+    int *__restrict__ first_occurences_end) {
 
   extern __shared__ char buffer[];
   unsigned int offset = 0;
@@ -22,18 +18,13 @@ __global__ void calculate_first_occurences_kernel_ptr(
   int block_start = blockIdx.x * NELEMENTS_PER_BLOCK;
 
   // load all elements of senderlist needed by block into shared memory
-  for (int i = threadIdx.x; i < NELEMENTS_PER_BLOCK + 1; i += blockDim.x)
-  {
+  for (int i = threadIdx.x; i < NELEMENTS_PER_BLOCK + 1; i += blockDim.x) {
     int idx = block_start + i;
 
-    if (idx < nelements_input)
-    {
-      if (use_sort)
-      {
+    if (idx < nelements_input) {
+      if (use_sort) {
         smem[i] = receiver_list[sort_idx[idx]];
-      }
-      else
-      {
+      } else {
         smem[i] = receiver_list[idx];
       }
     }
@@ -42,17 +33,14 @@ __global__ void calculate_first_occurences_kernel_ptr(
   __syncthreads();
 
   // deal with even boundaries
-  for (int i = 2 * threadIdx.x; i < NELEMENTS_PER_BLOCK; i += 2 * blockDim.x)
-  {
+  for (int i = 2 * threadIdx.x; i < NELEMENTS_PER_BLOCK; i += 2 * blockDim.x) {
     int idx = block_start + i;
 
-    if (idx + 1 < nelements_input)
-    {
+    if (idx + 1 < nelements_input) {
       int loc1 = smem[i];
       int loc2 = smem[i + 1];
 
-      if (loc1 != loc2)
-      {
+      if (loc1 != loc2) {
         first_occurences_end[loc1] = idx + 1;
         first_occurences_start[loc2] = idx + 1;
       }
@@ -60,26 +48,24 @@ __global__ void calculate_first_occurences_kernel_ptr(
   }
 
   // deal with odd boundaries
-  for (int i = 2 * threadIdx.x + 1; i < NELEMENTS_PER_BLOCK + 1; i += 2 * blockDim.x)
-  {
+  for (int i = 2 * threadIdx.x + 1; i < NELEMENTS_PER_BLOCK + 1;
+       i += 2 * blockDim.x) {
     int idx = block_start + i;
 
-    if (idx + 1 < nelements_input)
-    {
+    if (idx + 1 < nelements_input) {
       int loc1 = smem[i];
       int loc2 = smem[i + 1];
 
-      if (loc1 != loc2)
-      {
+      if (loc1 != loc2) {
         first_occurences_end[loc1] = idx + 1;
         first_occurences_start[loc2] = idx + 1;
       }
     }
   }
 
-  // deal with 0th and last element specifically, so we dont need to use torch::zeros
-  if (blockIdx.x == 0 && threadIdx.x == 0)
-  {
+  // deal with 0th and last element specifically, so we dont need to use
+  // torch::zeros
+  if (blockIdx.x == 0 && threadIdx.x == 0) {
     first_occurences_start[receiver_list[0]] = 0;
     first_occurences_end[receiver_list[nelements_input - 1]] = nelements_input;
   }
@@ -112,7 +98,6 @@ __global__ void inv_tp_kernel_ptr(
   scalar_t regX[TN] = {0.0};
   scalar_t regRadial[TM * TN] = {0.0};
   scalar_t regOut[TN * TM] = {0.0};
-  scalar_t regC[TN * TM] = {0.0};
   // check if this node has neighbours
   if (edge_end - edge_start == 0) {
     return;
@@ -136,7 +121,6 @@ __global__ void inv_tp_kernel_ptr(
 
       for (int i = 0; i < TN; i++) {
         for (int j = 0; j < TM; j++) {
-          regC[i * TM + j] = 0.0;
           regOut[i * TM + j] = 0.0;
         }
       }
@@ -162,11 +146,7 @@ __global__ void inv_tp_kernel_ptr(
 
         for (int i = 0; i < TN; i++) {
           for (int j = 0; j < TM; j++) {
-            scalar_t val = regX[i] * regY[j] * regRadial[i * TM + j];
-            scalar_t val_compensated = val - regC[i * TM + j];
-            scalar_t tmp_new = regOut[i * TM + j] + val_compensated;
-            regC[i * TM + j] = (tmp_new - regOut[i * TM + j]) - val_compensated;
-            regOut[i * TM + j] = tmp_new;
+            regOut[i * TM + j] += regX[i] * regY[j] * regRadial[i * TM + j];
           }
         }
       }
@@ -216,7 +196,6 @@ __global__ void backward_edge_inv_tp_kernel_ptr(
 
   scalar_t regX[TN] = {0.0};
   scalar_t regW[4 * TN] = {0.0};
-
   scalar_t regGradW[4 * TN] = {0.0};
 
   if (edge_end - edge_start == 0) {
@@ -332,7 +311,6 @@ __global__ void backward_node_inv_tp_kernel_ptr(
   scalar_t regRadial[TM * TN] = {0.0};
   scalar_t regGradIn[TM * TN] = {0.0};
   scalar_t regGradX[TN] = {0.0};
-  scalar_t regGradXC[TN] = {0.0};
 
   scalar_t *buffer_out =
       shared_array<scalar_t>(NWARPS_PER_BLOCK * WARP_SIZE, sptr, &space);
@@ -366,7 +344,6 @@ __global__ void backward_node_inv_tp_kernel_ptr(
 
     for (int i = 0; i < TN; i++) {
       regGradX[i] = 0.0;
-      regGradXC[i] = 0.0;
     }
 
     for (int edge = edge_start; edge < edge_end; edge++) {
@@ -380,7 +357,7 @@ __global__ void backward_node_inv_tp_kernel_ptr(
         }
 
         for (int j = 0; j < TM; j++) {
-          int lm_index = (int)sqrt((float) j * NWARPS_PER_BLOCK + m);
+          int lm_index = (int)sqrt((float)j * NWARPS_PER_BLOCK + m);
           for (int i = 0; i < TN; i++) {
             regRadial[i * TM + j] =
                 radial[sorted_id * 4 * nchannels + lm_index * nchannels +
@@ -395,12 +372,8 @@ __global__ void backward_node_inv_tp_kernel_ptr(
 
         for (int i = 0; i < TN; i++) {
           for (int j = 0; j < TM; j++) {
-            scalar_t val =
+            regGradX[i] +=
                 regGradIn[i * TM + j] * regRadial[i * TM + j] * regY[j];
-            scalar_t val_compensated = val - regGradXC[i];
-            scalar_t tmp_new = regGradX[i] + val_compensated;
-            regGradXC[i] = (tmp_new - regGradX[i]) - val_compensated;
-            regGradX[i] = tmp_new;
           }
         }
       }
